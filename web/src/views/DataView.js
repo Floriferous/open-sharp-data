30a38c58545261a063c7dc906d8c36f1d806fec9
Florian Bienefelt <florian.bienefelt@gmail.com>
2018-01-27 14:48:48 +0100

    Set up eslint and basic views

---
+++ web/src/views/DataView.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const DataView = props => <div>Data View!</div>;
+
+DataView.propTypes = {};
+
+export default DataView;
---
+++ web/src/views/HomeView.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const HomeView = props => <div>Home View</div>;
+
+HomeView.propTypes = {};
+
+export default HomeView;
