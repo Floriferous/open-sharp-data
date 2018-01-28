27c1380b5def4ad6d978ff712435970102e98668
Florian Bienefelt <florian.bienefelt@gmail.com>
2018-01-28 14:09:02 +0100

    Add footer to homepage

---
+++ web/src/views/HomeView/Footer.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const Footer = props => (
+  <p className="footer">Made with ❤️ in Switzerland - Dataset last uploaded on Jan 27th 2018</p>
+);
+
+Footer.propTypes = {};
+
+export default Footer;
--- web/src/views/HomeView/HomeView.js
+++ web/src/views/HomeView/HomeView.js
@@ -2,24 +2,28 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 import Actions from './Actions';
+import Footer from './Footer';
 import ColumnListModal from '../../components/ColumnListModal';
 import './HomeView.css';
 
 const HomeView = props => (
-  <section className="home-view card">
-    <h2>Open SHARP Data</h2>
-    <p>
-      This is a tool to let you get more insights into the SHARP dataset, we expect you to upload a
-      csv file containing 2 (and only 2) rows:
-    </p>
-    <ul>
-      <li>The first row should be the standard column labels of the SHARP Data set</li>
-      <li>The second row should be the survey data</li>
-      <li>
-        <ColumnListModal />
-      </li>
-    </ul>
-    <Actions />
+  <section className="home-view">
+    <div className="main">
+      <h2>Open SHARP Data</h2>
+      <p>
+        This is a tool to let you get more insights into the SHARP dataset, we expect you to upload
+        a csv file containing 2 (and only 2) rows:
+      </p>
+      <ul>
+        <li>The first row should be the standard column labels of the SHARP Data set</li>
+        <li>The second row should be the survey data</li>
+        <li>
+          <ColumnListModal />
+        </li>
+      </ul>
+      <Actions />
+    </div>
+    <Footer />
   </section>
 );
 
